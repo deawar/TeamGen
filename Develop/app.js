fbc91a3e181831509972c8ff31f8f660e5bcada1
dean <dean@SSCCBISHOP.LOCAL>
2020-05-10 00:36:44 -0400

    fixed github reg x

--- Develop/app.js
+++ Develop/app.js
@@ -84,7 +84,7 @@ const employeeQues = [
         validate: async (input) => {
             if (await input.trim().length === 0) {
                 return "NOT a valid entry!";
-            } else if (input.match(/^[0-9a-zA-Z]+$/i)) {
+            } else if (input.match(/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i)) {
                 return true;
             } else {
                 return "Please enter a valid GitHub Username!"
